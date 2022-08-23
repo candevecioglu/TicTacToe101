972d6db774c5d9339ee7fbf04dcf03a0d5bec53b
M. Can Devecioğlu <can.devecioglu@gmail.com>
2022-08-23 23:44:01 +0300

    First commit :)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TicTacToe101
+
+It will be classic tic-tac-toe game in Swift. Not ready, I'm working on it.
---
+++ TicTacToe101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe101
+//
+//  Created by M. Can Devecioğlu on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
